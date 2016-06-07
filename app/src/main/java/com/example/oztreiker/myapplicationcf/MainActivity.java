75264e8b58830a2bea9e9724428e3483d17445df
oztreiker <oztreiker@gmail.com>
2016-06-07 01:46:44 -0400

    EditText en accion

--- app/src/main/java/com/example/oztreiker/myapplicationcf/MainActivity.java
+++ app/src/main/java/com/example/oztreiker/myapplicationcf/MainActivity.java
@@ -2,19 +2,29 @@ package com.example.oztreiker.myapplicationcf;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView campo_texto;
+    EditText editable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //El campo_texto se va convertir en el R.id.texto que tengo en el activity_main.xml
-        campo_texto = (TextView) findViewById(R.id.texto);
+        /**
+         * El campo_texto se va convertir en el R.id.texto que tengo en el activity_main.xml
+         */
+        ////campo_texto = (TextView) findViewById(R.id.texto);
         //Con esta linea estamos editando lo que ya se tenia predeterminado en string.xml
-        campo_texto.setText("Hola de nuevo Oztreiker");
+        ////campo_texto.setText("Hola de nuevo Oztreiker");
+        /**
+         *El editable se va convertir en el R.id.edittext que tengo en el activity_main.xml
+         */
+        editable = (EditText) findViewById(R.id.edittext);
+        //obtendra el texto
+        editable.setText("Cambiamos texto");
     }
 }

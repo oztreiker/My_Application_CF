0c75db7581dcf2ff040147171d13649768c30c21
oztreiker <oztreiker@gmail.com>
2016-06-09 21:55:39 -0400

    My first steps with Toast testing two buttons

--- app/src/main/java/com/example/oztreiker/myapplicationcf/MainActivity.java
+++ app/src/main/java/com/example/oztreiker/myapplicationcf/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.oztreiker.myapplicationcf;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
     TextView campo_texto;
     EditText editable;
+    Button boton,botontoasting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         /**
          * El campo_texto se va convertir en el R.id.texto que tengo en el activity_main.xml
          */
-        ////campo_texto = (TextView) findViewById(R.id.texto);
+        campo_texto = (TextView) findViewById(R.id.texto);
         //Con esta linea estamos editando lo que ya se tenia predeterminado en string.xml
         ////campo_texto.setText("Hola de nuevo Oztreiker");
         /**
@@ -26,5 +31,36 @@ public class MainActivity extends AppCompatActivity {
         editable = (EditText) findViewById(R.id.edittext);
         //obtendra el texto
         editable.setText("Cambiamos texto");
+        /**
+         *
+         */
+        boton = (Button) findViewById(R.id.boton);
+        //botonN = (Button) findViewById(R.id.botonN);
+        boton.setOnClickListener(this);
+        //botonN.setOnClickListener(this);
+        /**
+         *
+         */
+        botontoasting = (Button) findViewById(R.id.botontoast);
+        botontoasting.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.boton:
+                //recuperamos el texto que contiene el editable y lo convertimos en cadena.
+                String dato = editable.getText().toString();
+                campo_texto.setText(dato);
+                Toast.makeText(getApplicationContext(),"SERA QUE SE MUESTRA ESTE TOAST",Toast.LENGTH_LONG).show();
+
+                break;
+            //case R.id.botonN:
+                //campo_texto.setText("Boton 2!!!");
+                //break;
+            case R.id.botontoast:
+                Toast.makeText(getApplicationContext(),"Esto es un toast de prueba",Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
